550a1cff45aae5361908ecdb6b53563eb37149fd
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 20:09:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,11 @@ def summon_captain_planet
     planeteer_calls = ("earth", "wind", "fire", "water", "heart")
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  when"returns true if any calls are longer than 4 characters" do
+  when  "returns false if all calls are 4 characters or less" do  
+    short_words = ("puff", "go", "two")
+    assorted_words = ("two", "go", "industrious", "bop")
 end
 
 def find_the_cheese# code an argument here
