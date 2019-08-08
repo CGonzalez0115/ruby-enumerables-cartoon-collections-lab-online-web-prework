29dea827b83197531bb876518878891ec961878e
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 20:03:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves
+  when "prints out the 7 dwarfs in a numbered list" do
+    roll_call_dwarves = ("Doc", "Dopey", "Bashful", "Grumpy")
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  
 end
 
 def long_planeteer_calls# code an argument here
